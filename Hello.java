60235ca02f24a5f8fb791eb6385f8adf044922d5
RAJ-MANE <mmane94@gmail.com>
2025-07-09 03:41:42 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -35,40 +35,4 @@ public class Hello {
         sc.close();
     }
 }
-import java.util.*;
 
-public class Hello {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int[] onearray = new int[4];
-        int[][] twoarray = new int[3][3];
-
-        System.out.println("Enter elements for one D array");
-        for (int i = 0; i < 4; i++) {
-            onearray[i] = sc.nextInt();
-        }
-
-        System.out.println("One D array elements are:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println(onearray[i]);
-        }
-
-        System.out.println("Enter elements for two D array");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                twoarray[i][j] = sc.nextInt();
-            }
-        }
-
-        System.out.println("Two D array elements are:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(twoarray[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        sc.close();
-    }
-}
